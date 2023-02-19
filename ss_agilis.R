fa735a16c1a1ac3329d83fc0a2e1ea9807bc77a6
Negylabavan <sikterrobert94@gmail.com>
2023-02-19 14:21:02 +0100

    practice agilis

---
+++ ss_agilis.R
@@ -0,0 +1,5 @@
+library(unmarked)
+library(AICcmodavg)
+
+dataFilePath = "C:/Users/4lábavan/OneDrive/Dokumentumok/Rcodes/occupancy_modelling/data/Table_S2.csv"
+dfAgilis = read.csv(dataFilePath, header = TRUE)
